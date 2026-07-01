3cef6d7f2ee9d9418eb51fb190f7df44cc749756
James <james.webb@cern.ch>
2026-07-01 11:05:25 +0200

    Update LinkDef for RecoTrack, RecoVertex vectors.

--- Examples/Algorithms/SHiP/LinkDef.h
+++ Examples/Algorithms/SHiP/LinkDef.h
@@ -2,4 +2,6 @@
 #pragma link C++ namespace ActsExamples;
 #pragma link C++ class ActsExamples::RecoTrack+;
 #pragma link C++ class ActsExamples::RecoVertex+;
+#pragma link C++ class std::vector<ActsExamples::RecoTrack>+;
+#pragma link C++ class std::vector<ActsExamples::RecoVertex>+;
 #endif
